7c2067a99878a5b0c0d985876c661ecca5af09ae
Michael Cribbin <michaelc@stripe.com>
2021-01-11 21:35:24 +0000

    Add  epsBank and types

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import {
   IbanElementComponent,
   IdealBankElementComponent,
   P24BankElementComponent,
+  EPSBankElementComponent,
   PaymentRequestButtonElementComponent,
 } from './types';
 
@@ -95,6 +96,14 @@ export const IdealBankElement: IdealBankElementComponent = createElementComponen
  */
 export const P24BankElement: P24BankElementComponent = createElementComponent(
   'p24Bank',
+  isServer
+);
+
+/**
+ * @docs https://stripe.com/docs/stripe-js/react#element-components
+ */
+export const EPSBankElement: EPSBankElementComponent = createElementComponent(
+  'epsBank',
   isServer
 );
 
